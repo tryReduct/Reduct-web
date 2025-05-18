f3610d5e4919f79b9a66a92d22833027d8bc79ea
Amaan Bilwar <bilwarad@mail.uc.edu>
2025-05-17 22:40:35 -0400

    test

--- frontend/src/components/SourceMonitor.tsx
+++ frontend/src/components/SourceMonitor.tsx
@@ -1,7 +1,26 @@
+import "@vidstack/react/player/styles/default/theme.css";
+import "@vidstack/react/player/styles/default/layouts/video.css";
+import { MediaPlayer, MediaProvider } from "@vidstack/react";
+import {
+  defaultLayoutIcons,
+  DefaultVideoLayout,
+} from "@vidstack/react/player/layouts/default";
+
 const SourceMonitor = () => {
-    return (
-      <div>source monitor</div>
-    )
-  }
-  
-  export default SourceMonitor
+  return (
+    <main>
+      <MediaPlayer
+        title="Sprite Fight"
+        src="https://files.vidstack.io/sprite-fight/720p.mp4"
+      >
+        <MediaProvider />
+        <DefaultVideoLayout
+          thumbnails="https://files.vidstack.io/sprite-fight/thumbnails.vtt"
+          icons={defaultLayoutIcons}
+        />
+      </MediaPlayer>
+    </main>
+  );
+};
+
+export default SourceMonitor;
